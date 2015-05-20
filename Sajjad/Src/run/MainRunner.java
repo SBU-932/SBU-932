905603219d380fb8eb86e2d82bb765c5eabd96f9
MCSH <MCSHEMAIL@GMAIL.COM>
2015-05-20 15:48:36 +0430

    Added window

--- Sajjad/Src/run/MainRunner.java
+++ Sajjad/Src/run/MainRunner.java
@@ -1,10 +1,11 @@
 package run;
 
+import window.GameWin;
+
 public class MainRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		GameWin g = new GameWin();
 	}
 
 }
