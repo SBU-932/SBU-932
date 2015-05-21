eb16ddb9c6f31a5f5978706d18f50a910d05e0b1
MCSH <MCSHEMAIL@GMAIL.COM>
2015-05-21 11:31:11 +0430

    Added Game panel

---
+++ Sajjad/Src/window/GamePanel.java
@@ -0,0 +1,16 @@
+package window;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public GamePanel() {
+		
+	}
+
+}
--- Sajjad/Src/window/GameWin.java
+++ Sajjad/Src/window/GameWin.java
@@ -11,13 +11,10 @@ public class GameWin extends JFrame {
 
 	private JPanel contentPane;
 
-	/**
-	 * Create the frame.
-	 */
 	public GameWin() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 800	, 600);
-		contentPane = new JPanel();
+		contentPane = new GamePanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
