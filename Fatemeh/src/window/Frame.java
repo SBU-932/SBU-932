f8c08c5cdedd9e307996533416acc4da6c20393b
fatemeh_bazdar <fatemeh_bazdar@yahoo.com>
2015-05-23 20:14:40 +0430

    fix

--- Fatemeh/src/window/Frame.java
+++ Fatemeh/src/window/Frame.java
@@ -9,7 +9,7 @@ import javax.swing.border.EmptyBorder;
 
 public class Frame extends JFrame {
 
-	private JPanel contentPane;
+	private Panel contentPane;
 
 	/**
 	 * Create the frame.
@@ -17,7 +17,7 @@ public class Frame extends JFrame {
 	public Frame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
+		contentPane = new Panel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
