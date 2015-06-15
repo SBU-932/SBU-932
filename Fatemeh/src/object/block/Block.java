5223a124ab4c1b3aca1d0c8c07acc4f3b16d587a
MCSH <MCSHEMAIL@GMAIL.COM>
2015-06-16 00:03:55 +0430

    balls, balls everywhere

--- Fatemeh/src/object/block/Block.java
+++ Fatemeh/src/object/block/Block.java
@@ -1,6 +1,5 @@
 package object.block;
 
-
 import game.State;
 
 import java.awt.Color;
@@ -10,10 +9,10 @@ import java.util.Random;
 
 import object.GameObj;
 import object.display.Shot;
-import objects.blocks.Pair;
 
 public class Block implements GameObj {
-	 private Random Rn;
+	private Random Rn;
+
 	public Block() {
 		// TODO Auto-generated constructor stub
 		State.getInstance().blocks = this;
@@ -22,16 +21,16 @@ public class Block implements GameObj {
 		State.getInstance().row = (State.getInstance().length / State
 				.getInstance().Nr);
 		State.getInstance().clom = (State.getInstance().width / State
-				.getInstance().Nc)-5;
+				.getInstance().Nc) - 5;
 
 		// meghdar dehi be array block
 		State.getInstance().block = new Colour[State.getInstance().Nr][State
 				.getInstance().Nc];
 
-		 Rn = new Random();
+		Rn = new Random();
 
 		for (int i = State.getInstance().Nr - 1; i >= 0; i--) {
-			int rn = Rn.nextInt(State.getInstance().Nr-10);
+			int rn = Rn.nextInt(State.getInstance().Nr - 10);
 			rn += 3;
 			for (int j = 0; j < rn; j++) {
 				State.getInstance().block[i][j] = Colour.getType(Rn.nextInt(5));
@@ -57,15 +56,15 @@ public class Block implements GameObj {
 
 		for (int i = State.getInstance().Nr - 1; i >= 0; i--) {
 			for (int j = State.getInstance().Nc - 1; j >= 0; j--) {
-				if (State.getInstance().block[i][j] != null){
+				if (State.getInstance().block[i][j] != null) {
 					g.setColor(State.getInstance().block[i][j].getColor());
 					g.fillRect(i * State.getInstance().row + 1,
 							j * State.getInstance().clom + 1,
 							State.getInstance().row - 1,
 							State.getInstance().clom - 1);
-			
+
 				}
-					
+
 			}
 		}
 	}
@@ -76,14 +75,74 @@ public class Block implements GameObj {
 		int y = ((int) shot.getY()) / State.getInstance().clom;
 		if (x >= 0 & y >= 0 & x < State.getInstance().Nr
 				& y < State.getInstance().Nc) {
-			if (State.getInstance().block[x][y] != null&& State.getInstance().
-					block[x][y].getColor().equals(shot.getcolor())) {
-				ArrayList <Pair<Integer,Integer>>check = new ArrayList <>();
-				ArrayList<Pair<Integer,Integer>>remove = new ArrayList<>();
-				check.add(new Pair<Integer,Integer>(x,y));
-				for(int i =0 ; i<check.size();i++){
-					int I=check.get(i).first,J=check.get(i).second;
-					remove.add(check.get(i))
+			if (State.getInstance().block[x][y] != null
+					&& State.getInstance().block[x][y].getColor().equals(
+							shot.getcolor())) {
+
+				ArrayList<Pair<Integer, Integer>> check = new ArrayList<>();
+				ArrayList<Pair<Integer, Integer>> remove = new ArrayList<>();
+
+				check.add(new Pair<Integer, Integer>(x, y));
+
+				for (int i = 0; i < check.size(); i++) {
+					int I = check.get(i).first, J = check.get(i).second;
+
+					try {
+						if (State.getInstance().block[I + 1][J] != null
+								&& State.getInstance().block[I + 1][J]
+										.getColor().equals(shot.getcolor()))
+							if (!has(check, I + 1, J))
+								check.add(new Pair<Integer, Integer>(I + 1, J));
+					} catch (IndexOutOfBoundsException e) {
+					}
+
+					try {
+						if (State.getInstance().block[I - 1][J] != null
+								&& State.getInstance().block[I - 1][J]
+										.getColor().equals(shot.getcolor()))
+							if (!has(check, I - 1, J))
+								check.add(new Pair<Integer, Integer>(I - 1, J));
+					} catch (IndexOutOfBoundsException e) {
+					}
+
+					try {
+						if (State.getInstance().block[I][J + 1] != null
+								&& State.getInstance().block[I][J + 1]
+										.getColor().equals(shot.getcolor()))
+							if (!has(check, I, J + 1))
+								check.add(new Pair<Integer, Integer>(I, J + 1));
+					} catch (IndexOutOfBoundsException e) {
+					}
+
+					try {
+						if (State.getInstance().block[I][J - 1] != null
+								&& State.getInstance().block[I][J - 1]
+										.getColor().equals(shot.getcolor()))
+							if (!has(check, I, J - 1))
+								check.add(new Pair<Integer, Integer>(I, J - 1));
+					} catch (IndexOutOfBoundsException e) {
+					}
+
+					remove.add(check.get(i));
+				}
+			}
+			
+			
+			
+			return true;
+		}
+		
+		return false;
+	}
+
+	private boolean has(ArrayList<object.block.Pair<Integer, Integer>> check,
+			int I, int J) {
+		for (int i = 0; i < check.size(); i++) {
+			if (check.get(i).first == I)
+				if (check.get(i).second == J)
+					return true;
+		}
+		return false;
 	}
 
 	@Override
